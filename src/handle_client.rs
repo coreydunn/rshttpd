60f0df58fb3d32a71520a18caacdc43ead50bf43
roswold <coreydunn92@outlook.com>
2021-04-12 10:04:16 -0500

    ignore empty GET requests, write_sock

--- src/handle_client.rs
+++ src/handle_client.rs
@@ -3,20 +3,26 @@ pub fn handle_client(client_sock:&mut std::net::TcpStream,addr:std::net::SocketA
     print!("Connected to client at {:?}\n",addr);
     //let mut v:Vec<u8>=vec!();
 
-    loop
+    'connection: loop
     {
         let mut request:String="".to_string();
         let mut sock_buffer:[u8;10]=[0,0,0,0,0,0,0,0,0,0];
 
         'read_output: while std::io::Read::read(client_sock,&mut sock_buffer).unwrap() > 0
         {
-            let st=std::str::from_utf8(&sock_buffer).expect("Found invalid UTF-8 string");
+            let sock_str_buffer=std::str::from_utf8(&sock_buffer).expect("Found invalid UTF-8 string");
 
-            request.push_str(st);
+            request.push_str(sock_str_buffer);
             if request.contains("\r\n\r\n")
             {
-                //let m:Vec<&str>=st.split("\n\n").collect();
+                //let m:Vec<&str>=sock_str_buffer.split("\n\n").collect();
                 break 'read_output;
+            }
+
+            // Ignore empty requests
+            if request.trim() == ""
+            {
+                continue 'connection;
             }
         }
 
@@ -24,10 +30,26 @@ pub fn handle_client(client_sock:&mut std::net::TcpStream,addr:std::net::SocketA
         let mut uri:String="".to_string();
         if request.len() > 0
         {
-            if &request[0..3]=="GET"
+
+            match &request[0..3]
             {
-                uri=std::iter::Iterator
-                    ::nth(&mut request.split(" "),1).unwrap().to_string();
+                //if &request[0..3]=="GET"
+                "GET" =>
+                {
+                    uri=std::iter::Iterator
+                        ::nth(&mut request.split(" "),1).unwrap().to_string();
+                    if uri.trim() == ""
+                    {
+                        uri="".to_string();
+                        continue 'connection;
+                    }
+                },
+
+                _ =>
+                {
+                    // Ignore unknown requests
+                    continue 'connection;
+                }
             }
         }
 
@@ -46,6 +68,11 @@ pub fn handle_client(client_sock:&mut std::net::TcpStream,addr:std::net::SocketA
             uri=uri_s.join("/");
         }
 
+        if uri.trim() == ""
+        {
+            continue 'connection;
+        }
+
         print!("GET: {:?}\t<=\t[peer: {}]\n",uri.as_str(),addr);
         //print!("GET: {:?}\t[peer: {}]\n",uri,addr);
 
@@ -56,9 +83,11 @@ pub fn handle_client(client_sock:&mut std::net::TcpStream,addr:std::net::SocketA
                 std::io::Read::read_to_string(&mut f,&mut uri_data).unwrap();
 
                 let response=format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",uri_data.len(),uri_data);
-                std::io::Write::write(client_sock,
-                                      response
-                                      .as_bytes()).unwrap();
+                if ! write_sock(client_sock,&addr,response.as_str())
+                {
+                    print!("Disconnected from client ({:?})\n",addr);
+                    break 'connection;
+                }
                 print!("200 OK: {:?}\t=>\t[peer: {}]\n",uri.as_str(),addr);
             },
 
@@ -74,30 +103,52 @@ pub fn handle_client(client_sock:&mut std::net::TcpStream,addr:std::net::SocketA
                         std::io::Read::read_to_string(&mut ff,&mut uri_data).unwrap();
 
                         let response=format!("HTTP/1.1 404 Not Found\r\nContent-Length: {}\r\n\r\n{}",uri_data.len(),uri_data);
-                        std::io::Write::write(client_sock,
-                                              response
-                                              .as_bytes()).unwrap();
+                        if ! write_sock(client_sock,&addr,response.as_str())
+                        {
+                            print!("Disconnected from client ({:?})\n",addr);
+                            break 'connection;
+                        }
                     },
 
                     // Otherwise display minimal built-in 404 message
                     Err(_) => {
                         uri_data="<html>404 Error: Not Found</html>".to_string();
                         let response=format!("HTTP/1.1 404 Not Found\r\nContent-Length: {}\r\n\r\n{}",uri_data.len(),uri_data);
-                        match std::io::Write::write(client_sock,
-                                              response
-                                              .as_bytes())
+
+                        if ! write_sock(client_sock,&addr,response.as_str())
                         {
-                            Ok(__) => (),
-                            Err(__) => {
-                                print!("Disconnected from client ({:?})\n",addr);
-                                return;
-                            }
+                            print!("Disconnected from client ({:?})\n",addr);
+                            break 'connection;
                         }
                     },
                 }
             },
 
         };
+    }
+
+    print!("successfully disconnected\n");
+    let msg=format!("error: failed to shutdown [peer: {}]\n",addr);
+    match client_sock.shutdown(std::net::Shutdown::Both)
+    {
+        Ok(_) => (),
+        Err(_) => {
+            print!("{}",msg)
+        }
+    }
+}
+
+fn write_sock(client_sock:&mut std::net::TcpStream,addr:&std::net::SocketAddr,response:&str) -> bool
+{
+    match std::io::Write::write(client_sock,
+                                response
+                                .as_bytes())
+    {
+        Ok(__) => true,
+        Err(__) => {
+            print!("Disconnected from client ({:?})\n",addr);
+            false
+        }
     }
 }
 
